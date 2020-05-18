6fda4f6246951fba4ef2b7985afe1a81b8855cab
Mikel2003 <MishaBobyM@gmail.com>
2020-05-18 17:58:56 +0300

    ActiveBox

---
+++ ActiveBox/app.js
@@ -0,0 +1,69 @@
+$(function() {
+
+    /* FIXED HEADER */
+
+    let header = $("#header");
+    let intro = $("#intro");
+    let introH = intro.innerHeight();
+    let scrollPos = $(window).scrollTop();
+    let nav = $("#nav")
+    let navToggle = $("#navToggle");
+
+    checkScroll(scrollPos, introH);
+
+    $(window).on("scroll resize", function() {
+        introH = intro.innerHeight();
+        scrollPos = $(this).scrollTop();
+
+        checkScroll(scrollPos, introH);
+
+    });
+
+    function checkScroll(scrollPos, introH) {
+
+        if( scrollPos > introH ) {
+            header.addClass("fixed");
+        } else {
+            header.removeClass("fixed");
+        }
+    }
+
+    /* SMOOTH SCROLL */
+
+    $("[data-scroll]").on("click", function(event) {
+        event.preventDefault();
+
+        let elementID = $(this).data('scroll');
+        let elementOffset = $(elementID).offset().top;
+
+        nav.removeClass("show")
+
+        console.log(elementOffset);
+
+        $("html, body").animate({
+            scrollTop: elementOffset -70
+        }, 700);
+    });
+
+    /* NAV TOGGLE */
+    navToggle.on("click", function(event) {
+        event.preventDefault();
+
+        nav.toggleClass("show");
+    })
+
+
+    /* REVIEWS */
+
+    let slider = $("#reviewsSlider");
+
+    slider.slick({
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      fade: false,
+      arrows: false,
+      dots: true
+});
+
+});
