3a2da6f1952fd8f8f30d60040ea71d5723f0a2b7
Michael Krilov <MishaBobyM@gmail.com>
2022-06-05 23:07:07 +0500

    feat: add portfolio

---
+++ Portfolio-orig/js/script.js
@@ -0,0 +1,13 @@
+VANTA.NET({
+  el: ".developer",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  spacing: 16.00,
+  color: 0xF6D42A,
+  backgroundColor: 0x0A0A0A,
+});
